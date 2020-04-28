5dd1191d040930f10450cf7593f1c255af598cf5
spencer-mcguire <smcguire1826@gmail.com>
2020-04-28 15:49:14 -0500

    dark mode

--- src/App.js
+++ src/App.js
@@ -6,20 +6,22 @@ import Login from './components/Login';
 import Register from './components/Register';
 import { Home } from './components/Home';
 
-import { ThemeProvider } from '@chakra-ui/core';
+import { ThemeProvider, ColorModeProvider } from '@chakra-ui/core';
 import customTheme from './theme';
 import './App.css';
 
 function App() {
   return (
     <ThemeProvider theme={customTheme}>
-      <div className='App'>
-        <Switch>
-          <Route exact path='/' component={Login} />
-          <Route path='/register' component={Register} />
-          <PrivateRoute path='/home' component={Home} />
-        </Switch>
-      </div>
+      <ColorModeProvider>
+        <div className='App'>
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path='/register' component={Register} />
+            <PrivateRoute path='/home' component={Home} />
+          </Switch>
+        </div>
+      </ColorModeProvider>
     </ThemeProvider>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Map } from './Map';
+import { Button, useColorMode, Flex } from '@chakra-ui/core';
 
 export const Home = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
-    <div>
-      <Map />
-    </div>
+    <Flex>
+      <header>
+        <Button onClick={toggleColorMode}>
+          Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
+        </Button>
+      </header>
+
+      {/* <Map /> */}
+    </Flex>
   );
 };
